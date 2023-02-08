08593482c42dd53233eb74aecc37aa6fa792cd7b
Shikhar Rai <tonyrai426@gmail.com>
2023-02-08 23:44:23 +0530

    Changes

--- app.js
+++ app.js
@@ -4,88 +4,35 @@ const menuItems = document.querySelectorAll(".menuItem");
 const products = [
   {
     id: 1,
-    title: "Air Force",
+    title: "Rucksacks",
     price: 119,
-    colors: [
-      {
-        code: "black",
-        img: "./img/GPS.mp4",
-      },
-      {
-        code: "darkblue",
-        img: "./img/air2.png",
-      },
-    ],
   },
   {
     id: 2,
-    title: "Air Jordan",
+    title: "Knives",
     price: 149,
-    colors: [
-      {
-        code: "lightgray",
-        img: "./img/jordan.png",
-      },
-      {
-        code: "green",
-        img: "./img/jordan2.png",
-      },
-    ],
   },
   {
     id: 3,
-    title: "Blazer",
+    title: "Binoculars",
     price: 109,
-    colors: [
-      {
-        code: "lightgray",
-        img: "./img/blazer.png",
-      },
-      {
-        code: "green",
-        img: "./img/blazer2.png",
-      },
-    ],
   },
   {
     id: 4,
-    title: "Crater",
+    title: "GPS",
     price: 129,
-    colors: [
-      {
-        code: "black",
-        img: "./img/crater.png",
-      },
-      {
-        code: "lightgray",
-        img: "./img/crater2.png",
-      },
-    ],
   },
   {
     id: 5,
-    title: "Hippie",
+    title: "Cameras",
     price: 99,
-    colors: [
-      {
-        code: "gray",
-        img: "./img/hippie.png",
-      },
-      {
-        code: "black",
-        img: "./img/hippie2.png",
-      },
-    ],
   },
 ];
 
 let choosenProduct = products[0];
 
-const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
 const currentProductPrice = document.querySelector(".productPrice");
-const currentProductColors = document.querySelectorAll(".color");
-const currentProductSizes = document.querySelectorAll(".size");
 
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
@@ -104,23 +51,6 @@ menuItems.forEach((item, index) => {
     currentProductColors.forEach((color, index) => {
       color.style.backgroundColor = choosenProduct.colors[index].code;
     });
-  });
-});
-
-currentProductColors.forEach((color, index) => {
-  color.addEventListener("click", () => {
-    currentProductImg.src = choosenProduct.colors[index].img;
-  });
-});
-
-currentProductSizes.forEach((size, index) => {
-  size.addEventListener("click", () => {
-    currentProductSizes.forEach((size) => {
-      size.style.backgroundColor = "white";
-      size.style.color = "black";
-    });
-    size.style.backgroundColor = "black";
-    size.style.color = "white";
   });
 });
 
